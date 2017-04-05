1e10492c57bc3df2f93219cc29abbe215b4c0269
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-04 19:30:20 -0600

    feat(tree-node): drop nodes to the end of the the children when dropping on a node

--- lib/models/tree-node.model.ts
+++ lib/models/tree-node.model.ts
@@ -160,7 +160,7 @@ export class TreeNode implements ITreeNode {
   onDrop($event) {
     this.mouseAction('drop', $event.event, {
       from: $event.element,
-      to: { parent: this, index: 0, dropOnNode: true }
+      to: { parent: this, index: (this.children && this.children.length) || 0, dropOnNode: true }
     });
   }
 
@@ -171,7 +171,6 @@ export class TreeNode implements ITreeNode {
   allowDrag() {
     return this.options.allowDrag(this);
   }
-
 
   // helper methods:
   loadNodeChildren() {
